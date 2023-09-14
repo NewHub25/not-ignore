ee7716e4ec9236bcd4c3e9f4fabbf247c88f753c
Terry <master_correo144@hotmail.com>
2023-09-14 08:44:40 -0500

    ADD error page

--- src/components/error-page.jsx
+++ src/components/error-page.jsx
@@ -1,7 +1,67 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "styled-components";
+
 export const ErrorPage = () => {
   return (
-    <>
-      <h1>Error, ups, falta pues. 🥑</h1>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        p: 10,
+        boxSizing: "border-box",
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: { xs: "none", md: "flex" } }}
+        >
+          <Grid item={true} xs={6}>
+            <ComponentToGrid />
+          </Grid>
+          <Grid item={true} xs={6}>
+            <img src="/404.png" alt="error 404" width={200} />
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: { xs: "flex", md: "none" } }}
+        >
+          <Grid>
+            <ComponentToGrid />
+          </Grid>
+          <Grid>
+            <img src="/404.png" alt="error 404" width={200} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
+
+function ComponentToGrid() {
+  const toggleTheme = useContext(ThemeContext);
+
+  return (
+    <>
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h6">
+        La página que estás buscando no existe
+        {/* The page you’re looking for doesn’t exist. */}
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: toggleTheme.principal }}
+      >
+        <Link to="/">Back Home</Link>
+      </Button>
+    </>
+  );
+}

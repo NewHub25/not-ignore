8d7ea334032d78cabe716a91237c106a25500144
Terry <master_correo144@hotmail.com>
2023-10-01 12:45:17 -0500

    ADD better regex to extract url

--- src/logic/extract-video-id.js
+++ src/logic/extract-video-id.js
@@ -1,9 +1,10 @@
 const extractVideoId = (url) => {
-  let [queryDomain, idYouTube, seconds] = url.split(/(?<==)/);
-  if (seconds) {
-    idYouTube = idYouTube.replace("=", "");
-  }
-
-  return { queryDomain, idYouTube };
+  // https://www.youtube.com/watch?v=iLmBy-HKIAw&list=WL&index=2
+  // https://youtu.be/iLmBy-HKIAw?si=B82xaXgQArZbhKSW
+  const result = url.match(
+    /https:\/\/(www\.)?\w+\.\w+\/(\w+\?v=)?([a-zA-Z-]+)/
+  );
+  return { url: result[0], idYouTube: result[3] };
 };
+
 export default extractVideoId;
